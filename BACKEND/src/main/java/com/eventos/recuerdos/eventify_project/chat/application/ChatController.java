1a247013bde34866eb6c8a601a21725cea1bc7fd
Giordano Fuentes <giordimega@hotmail.com>
2024-11-30 15:53:33 -0500

    crapetas creadas

---
+++ BACKEND/src/main/java/com/eventos/recuerdos/eventify_project/chat/application/ChatController.java
@@ -0,0 +1,10 @@
+package com.eventos.recuerdos.eventify_project.chat.application;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chats")
+public class ChatController {
+}
---
+++ BACKEND/src/main/java/com/eventos/recuerdos/eventify_project/chat/domain/ChatService.java
@@ -0,0 +1,4 @@
+package com.eventos.recuerdos.eventify_project.chat.domain;
+
+public class ChatService {
+}
